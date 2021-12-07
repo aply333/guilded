e34da19faa3c9d4f968b071cdc44cff35a4125e6
Andrei P Lysenko <andrei@aply.tech>
2021-12-07 01:04:39 -0800

    Layout - Functionality - Sign up - Tweaked layout, not a final product but does provide a good visual indication towards it. - Installed and began basic setup of react-hook-form; inputs now console log their stored values. - Began building out signup form. I may have to create a parent component to manage which set of input fields are displayed.

--- guilded/src/pages/landing/loginForm.jsx
+++ guilded/src/pages/landing/loginForm.jsx
@@ -1,13 +1,27 @@
+import {useForm} from "react-hook-form";
+import RegisterForm from "./registerForm";
+
 function LoginForm(){
+    const signUpClick = e =>{
+        e.preventDefault()
+
+    }
+    const {register, handleSubmit} = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    }
+
     return(
         <>
-        <form className="login">
+        <RegisterForm/>
+        <form onSubmit={handleSubmit(onSubmit)} className="login">
+            <button onClick={e=>signUpClick(e)} className="signupBtn">SIGN UP</button>
             <div className="formDecoration">
                 <hr />
             </div>
-            <input type='email'/>
-            <input type='password'/>
-            <button type='submit'>Login</button>
+            <input {...register("email", {required:true})} type='email'/>
+            <input {...register("password", {required:true})} type='password'/>
+            <button className="loginBtn" type='submit'>Login</button>
         </form>
         </>
     )
---
+++ guilded/src/pages/landing/registerForm.jsx
@@ -0,0 +1,15 @@
+function RegisterForm(){
+    return(
+        <>
+        <form className="login">
+            <input type="text" className="tag" />
+            <input type="text" className="name" />
+            <input type="email" className="email" />
+            <input type="password" className="password" />
+            <input type="text" className="password" />
+        </form>
+        </>
+    )
+}
+
+export default RegisterForm;
